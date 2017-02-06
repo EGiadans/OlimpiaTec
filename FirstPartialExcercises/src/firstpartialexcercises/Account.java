96d0fb984428cfb07768a1ad596d086e279b9298
Eduardo <Eduardo@LenovoGiadns>
2017-02-05 23:40:17 -0600

    Exsercise 6

--- FirstPartialExcercises/src/firstpartialexcercises/Account.java
+++ FirstPartialExcercises/src/firstpartialexcercises/Account.java
@@ -9,7 +9,7 @@ package firstpartialexcercises;
  *
  * @author Eduardo
  */
-public class Account {
+public class Account { //Atributes
     private String id;
     private String name;
     private int balance=0;
@@ -22,6 +22,7 @@ public class Account {
         this.name=name;
         this.balance=0;
     }
+    //Getters and Setters
     public String getId(){
         return this.id;
     }
@@ -31,20 +32,20 @@ public class Account {
     public int getBalance(){
         return this.balance;
     }
-    public int credit(int amount){
+    public int credit(int amount){ //method credit increases the balance with the given amount
         this.balance+=amount;
         return this.balance;
     }
-    public int debit(int amount){
-        if (amount<=this.balance){
+    public int debit(int amount){ //method debit does the opposite
+        if (amount<=this.balance){ //you cannot withdraw more than your balance
             this.balance-=amount;
         }else{
             System.out.println("Amount exceeded balance");
         }
         return this.balance;
     }
-    public int transferTo(Account another,int amount){
-        if (amount<=this.balance){
+    public int transferTo(Account another,int amount){ //transference method
+        if (amount<=this.balance){  //between different account instances
             another.credit(amount);
             this.balance-=amount;
         }else{
