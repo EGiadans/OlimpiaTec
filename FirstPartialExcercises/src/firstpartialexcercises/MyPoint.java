08a69fa2fa3d0da3f1bc89d59c647e8a1f57afa8
Eduardo <Eduardo@LenovoGiadns>
2017-02-05 23:43:49 -0600

    Exsercise 11

--- FirstPartialExcercises/src/firstpartialexcercises/MyPoint.java
+++ FirstPartialExcercises/src/firstpartialexcercises/MyPoint.java
@@ -9,10 +9,10 @@ package firstpartialexcercises;
  *
  * @author Eduardo
  */
-public class MyPoint {
+public class MyPoint { //Atributes
     private int x;
     private int y;
-    
+    //Constructors
     public MyPoint(){
         this.x=0;
         this.y=0;
@@ -21,6 +21,7 @@ public class MyPoint {
         this.x=x;
         this.y=y;
     }
+    //Getters and Setters
     public int getX(){
         return this.x;
     }
@@ -33,7 +34,7 @@ public class MyPoint {
     public void setY(int y){
         this.y=y;
     }
-    public int[] getXY(){
+    public int[] getXY(){ //Getter for an array form of our coordinate
         int[] arreglo= new int[2];
         arreglo[0]=this.x;
         arreglo[1]=this.y;
@@ -47,17 +48,17 @@ public class MyPoint {
     public String toString(){
         return "("+this.x+","+this.y+")";
     }
-    public double distance(int x, int y){
-        double dist;
-        dist=Math.sqrt(Math.pow(x-this.x,2)+Math.pow(y-this.y,2));
+    public double distance(int x, int y){ //calculation of distance from this point to a given 
+        double dist;                        //set of coordinates
+        dist=Math.sqrt(Math.pow(x-this.x,2)+Math.pow(y-this.y,2)); 
         return dist;
     }
-    public double distance(MyPoint another){
-        double dist;
+    public double distance(MyPoint another){ //calculation of distance from our point 
+        double dist;                    //to another instance of point
         dist=Math.sqrt(Math.pow(another.x-this.x,2)+Math.pow(another.y-this.y,2));
         return dist;
     }
-    public double distance(){
+    public double distance(){ //calculation from our point to the origin (0,0)
         double dist;
         dist=Math.sqrt(Math.pow(0-x,2)+Math.pow(0-y,2));
         return dist;
