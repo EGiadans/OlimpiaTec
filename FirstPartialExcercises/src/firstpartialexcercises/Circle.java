59dc2f7d9c0c9545d96af276bcd3234650fa45a0
Eduardo <Eduardo@LenovoGiadns>
2017-02-05 23:38:28 -0600

    Exsercise 2

--- FirstPartialExcercises/src/firstpartialexcercises/Circle.java
+++ FirstPartialExcercises/src/firstpartialexcercises/Circle.java
@@ -10,25 +10,25 @@ package firstpartialexcercises;
  * @author Eduardo
  */
 public class Circle {
-    private double radius;
-    
+    private double radius; //Atribute
+    //Constructors
     public Circle(){
         this(1.0);
     }
-    
     public Circle(double radius){
         this .radius=radius;
     }
+    //Getters and Setters
     public double getRadius(){
         return this.radius;
     }
     public void setRadius(double radius){
         this.radius=radius;
     }
-    public double getArea(){
+    public double getArea(){ //method for the area obtention
         return this.radius*this.radius*Math.PI;
     }
-    public double getCircumference(){
+    public double getCircumference(){//method for the circumference obtention
         return this.radius*2*Math.PI;
     }
     @Override
