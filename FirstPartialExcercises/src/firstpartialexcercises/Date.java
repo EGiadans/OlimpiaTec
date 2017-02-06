75c1286e48cc12b68d208959cd9f33fdb4cde120
Eduardo <Eduardo@LenovoGiadns>
2017-02-05 23:40:46 -0600

    Exsercise 7

--- FirstPartialExcercises/src/firstpartialexcercises/Date.java
+++ FirstPartialExcercises/src/firstpartialexcercises/Date.java
@@ -9,17 +9,17 @@ package firstpartialexcercises;
  *
  * @author Eduardo
  */
-public class Date {
+public class Date { //Atributes
     private int day;
     private int month;
     private int year;
-    
-    public Date(int day, int month, int year){
+    //Constructor
+    public Date(int day, int month, int year){ 
         this.day=day;
         this.month=month;
         this.year=year;
     }
-    
+    //Getters and Setters
     public int getDay(){
         return this.day;
     }
@@ -38,13 +38,13 @@ public class Date {
     public void setYear(int year){
         this.year=year;
     }
-    public void setDate(int day, int month, int year){
+    public void setDate(int day, int month, int year){ //Setter for the complete Date value
         this.day=day;
         this.month=month;
         this.year=year;
     }
     @Override
-    public String toString(){
+    public String toString(){ //We use this function to give a format to our values
         return String.format("%02d", this.day)+"/"+
                 String.format("%02d",this.month)+"/"+
                 String.format("%04d", this.year);
